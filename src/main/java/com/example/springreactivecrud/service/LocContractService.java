24bb63bb262ab3214e770d9954b374539a65a02d
User <articzarik@bk.ru>
2022-02-25 13:32:32 +0300

    commit 2

---
+++ src/main/java/com/example/springreactivecrud/service/LocContractService.java
@@ -0,0 +1,28 @@
+package com.example.springreactivecrud.service;
+
+import com.example.springreactivecrud.entity.LocContract;
+import com.example.springreactivecrud.repository.LocContractRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class LocContractService {
+    private final LocContractRepository locContractRepository;
+
+    public Flux<LocContract> getAll() {
+        return locContractRepository.findAll();
+    }
+        public Mono<LocContract> saveLocContract(LocContract locContract){
+            return locContractRepository.save(locContract);
+        }
+    public Flux<LocContract> getById(Long id){
+        return locContractRepository.getById(id);
+    }
+    public Mono<Void> deleteById(Long id){ return  locContractRepository.deleteById(id);}
+   public Flux<LocContract> getFirst10Id(){
+        return locContractRepository.findByIdLessThanEqual();
+   }
+}
