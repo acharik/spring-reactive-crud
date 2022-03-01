1e039f3749aaa508d0dfc250aed5ab8d625b962a
User <articzarik@bk.ru>
2022-03-01 14:42:08 +0300

    add Exception handler

--- src/main/java/com/example/springreactivecrud/exсeptions/ExceptionAdvice.java
+++ src/main/java/com/example/springreactivecrud/exсeptions/ExceptionAdvice.java
@@ -1,21 +1,12 @@
-package com.example.springreactivecrud.exeptions;
+package com.example.springreactivecrud.exсeptions;
 
-import com.example.springreactivecrud.entity.LocContract;
-import com.example.springreactivecrud.service.LocContractService;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import io.r2dbc.spi.R2dbcDataIntegrityViolationException;
-import org.apache.commons.logging.Log;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.codec.DecodingException;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.io.IOException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice()
@@ -38,4 +29,5 @@ public class ExceptionAdvice {
     public ResponseEntity<String> handleNonNullEx(DecodingException ex){
        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Обязательные поля должны быть заполнены");
     }
+
 }
