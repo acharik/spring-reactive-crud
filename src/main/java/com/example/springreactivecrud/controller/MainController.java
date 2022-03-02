9964d8a66795e6ca005a219b1f5ed77cd2838854
User <articzarik@bk.ru>
2022-03-02 12:18:24 +0300

    add http status

--- src/main/java/com/example/springreactivecrud/controller/MainController.java
+++ src/main/java/com/example/springreactivecrud/controller/MainController.java
@@ -5,6 +5,7 @@ import com.example.springreactivecrud.service.LocContractService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -20,31 +21,37 @@ public class MainController {
     private final LocContractService locContractService;
 
     @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<LocContract> add ( @RequestBody LocContract locContract)  {
 
             return locContractService.saveLocContract(locContract);
     }
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
     public Flux<LocContract> getById(@PathVariable("id") Long id)  {
 
         return locContractService.getById(id);}
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Mono<Void> deleteById(@PathVariable("id") Long id){
 
         return locContractService.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     Mono<LocContract> updateLocContract(@RequestBody LocContract locContract, @PathVariable("id") Long id){
         locContract.setId(id);
         return locContractService.saveLocContract(locContract);
     }
 
     @GetMapping(params = "num")
+    @ResponseStatus(HttpStatus.OK)
     public Mono<LocContract> getContractByNumber(@RequestParam String num){
         return locContractService.getContractByNum(num);
     }
     @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public Flux<LocContract> getContract(@RequestParam(required = false)
                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                                           @RequestParam(required = false) Long count){
